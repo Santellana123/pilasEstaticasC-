e1fb2349a2d87f0ecb931f8ef33b741c799f61c7
Santellana179 <imjuan179@gmail.com>
2023-12-04 21:40:54 -0600

    Agregar archivos de proyecto.

---
+++ pilasEstaticasC#/Pila.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pilasEstaticasC_
+{
+    internal class Pila
+    {
+        private const int MAX_SIZE = 10;
+        private int[] arreglo = new int[MAX_SIZE];
+        private int tope = -1;
+
+        public bool EstaVacia()
+        {
+            return tope == -1;
+        }
+
+        public bool EstaLlena()
+        {
+            return tope == MAX_SIZE - 1;
+        }
+
+        public void Push(int elemento)
+        {
+            if (!EstaLlena())
+            {
+                arreglo[++tope] = elemento;
+                Console.WriteLine($"Se ha insertado {elemento} en la pila.");
+            }
+            else
+            {
+                Console.WriteLine("La pila está llena. No se puede insertar.");
+            }
+        }
+
+        public void Pop()
+        {
+            if (!EstaVacia())
+            {
+                Console.WriteLine($"Se ha eliminado {arreglo[tope--]} de la pila.");
+            }
+            else
+            {
+                Console.WriteLine("La pila está vacía. No se puede eliminar.");
+            }
+        }
+
+        public int Top()
+        {
+            if (!EstaVacia())
+            {
+                return arreglo[tope];
+            }
+            else
+            {
+                Console.WriteLine("La pila está vacía. No hay elemento superior.");
+                return -1; // Valor sentinela para indicar que la pila está vacía
+            }
+        }
+    }
+}
+
---
+++ pilasEstaticasC#/Program.cs
@@ -0,0 +1,56 @@
+﻿using pilasEstaticasC_;
+
+Pila miPila = new Pila();
+
+while (true)
+{
+    Console.WriteLine("Seleccione una operación:");
+    Console.WriteLine("1. Push");
+    Console.WriteLine("2. Pop");
+    Console.WriteLine("3. Elemento Superior");
+    Console.WriteLine("4. Salir");
+
+    if (int.TryParse(Console.ReadLine(), out int opcion))
+    {
+        switch (opcion)
+        {
+            case 1:
+                Console.WriteLine("Ingrese el elemento a insertar:");
+                if (int.TryParse(Console.ReadLine(), out int elementoPush))
+                {
+                    miPila.Push(elementoPush);
+                }
+                else
+                {
+                    Console.WriteLine("Entrada no válida. Ingrese un número entero.");
+                }
+                break;
+
+            case 2:
+                miPila.Pop();
+                break;
+
+            case 3:
+                int elementoSuperior = miPila.Top();
+                if (elementoSuperior != -1)
+                {
+                    Console.WriteLine($"Elemento superior de la pila: {elementoSuperior}");
+                }
+                break;
+
+            case 4:
+                Environment.Exit(0);
+                break;
+
+            default:
+                Console.WriteLine("Opción no válida. Intente de nuevo.");
+                break;
+        }
+    }
+    else
+    {
+        Console.WriteLine("Entrada no válida. Ingrese un número entero.");
+    }
+
+    Console.WriteLine();
+}
